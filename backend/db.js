c955781099fe7591420c1714025b889be3dfff69
Balar Crens <balarcrens@gmail.com>
2025-04-28 15:33:30 +0530

    first commit

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const connectMongo = () => {
     mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
         serverSelectionTimeoutMS: 15000,
     }).then(() => console.log('MongoDB Connected'))
         .catch(err => console.error('MongoDB Error:', err));

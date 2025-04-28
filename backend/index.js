96bf702e3d1c7d3b392f4551224ef83dd72672a6
Balar Crens <balarcrens@gmail.com>
2025-04-28 14:48:44 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const connectMongo = require('./db.js');
 const express = require('express');
 const cors = require('cors');
-
+const port = process.env.PORT || 7000;
 const app = express();
 
 connectMongo();
@@ -13,4 +13,4 @@ app.use('/api/auth', require('./routes/auth'));
 
 app.use('/api/product', require('./routes/product'));
 
-app.listen(7000)
+app.listen(port)
